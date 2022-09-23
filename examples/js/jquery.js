4b4520630bd60a1ec4a0958f0e4e1394f874176a
Annamarie Cruz Flores <am.cruzflores@gmail.com>
2022-09-23 12:29:51 -0700

    Added Tab Group

--- examples/js/jquery.js
+++ examples/js/jquery.js
@@ -9,9 +9,13 @@
 
 //});
 
-//ACCORDIAN
+
+
+
     $(()=>{
 
+
+    //ACCORDIAN
         $(".accordian dt").on("click", function(e) {
         
         //$(this).next().show();
@@ -27,6 +31,25 @@
     
         });
    
+
+
+    /*TAB GROUP*/
+
+    $(".tabgroup .tab").on("click", function(e) {
+        let id = $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active")
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active")
+    })
+
+
+
+
+
     });
 
 
